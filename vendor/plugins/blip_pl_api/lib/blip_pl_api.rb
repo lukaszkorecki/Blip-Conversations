26dd165130cb5ae4ff32712a3342d7b018cb83e2
Łukasz Korecki <lukasz@coffeesounds.com>
2010-01-11 22:43:38 +0000

    Some simple tests for the blip plugin

--- vendor/plugins/blip_pl_api/lib/blip_pl_api.rb
+++ vendor/plugins/blip_pl_api/lib/blip_pl_api.rb
@@ -1,16 +1,17 @@
-require 'net/http'
-require 'uri'
 # BlipPlApi
 class BlipPlApi
-  attr_accessor :api_root
-  @@api_root = 'http://api.blip.pl'
-  def send_request(path)
-    url= URI.parse(@@api_root + path.join('/'))
-    res = HTTP.get_print(url)
+  attr_reader :api_root
+  def initialize
+    @api_root = 'http://api.blip.pl'
   end
- 
-  
+  def get_request( path)
+    require 'net/http'
+    require 'uri'
+    res = Net::HTTP.get_response(URI.parse(@api_root + path))
+    res.inspect
+    res.body
+  end
   def get_status_by_id(id)
-    self.send_request(:get, { :updates => id})
+    self.get_request('/updates/'+id.to_s)
   end
 end
