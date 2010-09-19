1958c6af266402953fa68e65bb0ecf591334f9ef
Łukasz Korecki <lukasz@coffeesounds.com>
2010-09-19 13:49:26 +0100

    updates

--- app/helpers/threads_helper.rb
+++ app/helpers/threads_helper.rb
@@ -1,9 +1,6 @@
 module ThreadsHelper
   def parse_blip_body body
-    body.gsub! /http:\S+/, '<a href=\0>\0</a>'
-    body.gsub! /\^\W+/, '<a href="http://blip.pl/"\0>\0</a>'
-    body.gsub! /\#\W+/, '<a href="http://blip.pl/tags/\0">\0</a>'
-    return body
+    Formatter.htmlize_body body
   end
 
 end
